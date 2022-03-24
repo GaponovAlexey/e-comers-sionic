e5a338f3f7949ca864683d61a93b94252fb102d4
Kekelberi <alexei.gaponov@gmail.com>
2022-03-24 19:23:43 +0300

    save

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,9 +1,33 @@
+import Image from 'next/image'
 import style from './layout.module.scss'
 
 const Footer = () => {
   return (
-    <div className={style.footer} >
+    <div className={style.footer}>
       <h1>React</h1>
+      <div className={style.social} >
+        <div>
+          <p>Присоединяйтесь к нам</p>
+          <Image src='/svg/Facebook.svg' width='28px' height='28px' alt='f' />
+          <Image src='/svg/Vk.svg' width='28px' height='28px' alt='VK' />
+          <Image src='/svg/Inst.svg' width='28px' height='28px' alt='Inst' />
+        </div>
+        <div>
+          <p>Устанавливайте приложение</p>
+          <Image
+            src='/svg/Google.svg'
+            width='104px'
+            height='32px'
+            alt='Google'
+          />
+          <Image
+            src='/svg/AppStore.svg'
+            width='104px'
+            height='32px'
+            alt='AppStore'
+          />
+        </div>
+      </div>
     </div>
   )
 }
